4a8dc6c49dea9310c457453d649e453e06dc66cb
Ethan Wang <ethanwang091014@gmail.com>
2026-06-24 14:53:10 -0400

    feat: Implement core module system - Develop ModuleSystem class storing attached module instances - Update tests

--- src/ship.py
+++ src/ship.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from enum import Enum, auto
+from typing import Dict, Optional, Type
 
 
 class Classification(Enum):
@@ -9,11 +10,164 @@ class Classification(Enum):
     BATTLESHIP = auto()
 
 
+class Module(ABC):
+    """Base class for ship modules which determine ship statistics"""
+    _name: str
+    _allowed_ships: list[Classification]
+
+    @abstractmethod
+    def __init__(self, name: str, allowed_ships: list[Classification]):
+        self._name = name
+        self._allowed_ships = allowed_ships
+
+    @property
+    def name(self)->str:
+        return self._name
+    
+    @property
+    def allowed_ships(self)->list[Classification]:
+        return self._allowed_ships
+    
+    def __str__(self)->str:
+        return self.name
+    
+    @abstractmethod
+    def __repr__(self)->str:
+        return f'Module(name={self.name}, allowed_ships={self.allowed_ships})'
+
+
+class Armament(Module):
+    """Class for modules that fulfill the Armament slots"""
+    _damage: int = 0
+
+    def __init__(self, name: str, allowed_ships: list[Classification], damage: int):
+        super().__init__(name, allowed_ships)
+        self._damage = damage 
+    
+    @property
+    def damage(self)->int:
+        return self._damage
+    
+    def __repr__(self)->str:
+        return f'Armament(name={self.name!r}, allowed_ships={self.allowed_ships!r}, damage={self.damage}'
+
+
+class Engine(Module):
+    """Class for modules that fulfill the engine slot"""
+    _propulsion: int = 0
+
+    def __init__(self, name: str, allowed_ships: list[Classification], propulsion: int):
+        super().__init__(name, allowed_ships)
+        self._propulsion = propulsion
+    
+    @property
+    def propulsion(self)->int:
+        return self._propulsion
+    
+    def __repr__(self)->str:
+        return f'Engine(name={self.name!r}, allowed_ships={self.allowed_ships!r}, propulsion={self.propulsion})'
+
+
+class Armor(Module):
+    """Class for modules that fulfill the armor slot"""
+    _armor: int =  0
+
+    def __init__(self, name: str, allowed_ships: list[Classification], protection: int):
+        super().__init__(name, allowed_ships)
+        self._protection = protection
+
+    @property
+    def protection(self)->int:
+        return self._protection
+    
+    def __repr__(self)->str:
+        return f'Armor(name={self.name!r}, allowed_ships={self.allowed_ships!r}, protection={self.protection})'
+    
+
+class ModuleSystem(ABC):
+    """Base class for ship module systems"""
+    _designated_classification: Classification
+    _SLOT_CONFIG: Dict[str, Type[Module]]
+
+    @abstractmethod
+    def __init__(self, classification: Classification):
+        self._designated_classification = classification
+        self._equipped_modules: Dict[str, Optional[Module]] = {slot_name: None for slot_name in self.SLOT_CONFIG}
+
+    def equip(self, module: Module, target_slot: str)->bool:
+        if self._designated_classification not in module.allowed_ships:
+            print("Wrong ship type")
+            return False
+        if target_slot not in self.SLOT_CONFIG:
+            print("Slot not found")
+            return False
+        if not isinstance(module, self.SLOT_CONFIG[target_slot]):
+            print("Wrong equipment type")
+            return False
+
+        self._equipped_modules[target_slot] = module
+        return True
+
+    @property
+    def designated_classification(self)->Classification:
+        return self._designated_classification
+
+    @property
+    def SLOT_CONFIG(self)->Dict[str, Type[Module]]:
+        return self._SLOT_CONFIG
+
+    @property
+    def equipped_modules(self)->Dict[str, Optional[Module]]:
+        return self._equipped_modules
+    
+    def __repr__(self)->str:
+        return f'ModuleSystem(designated_classification={self.designated_classification}, SLOT_CONFIG={self.SLOT_CONFIG}), equipped_modules={self.equipped_modules}'
+
+
+class DestroyerModuleSystem(ModuleSystem):
+    """Module system for destroyers"""
+    _SLOT_CONFIG = {
+        "engine": Engine,
+        "armor": Armor,
+        "armament_one": Armament,
+        "armament_two": Armament
+    }
+
+    def __init__(self):
+        super().__init__(Classification.DESTROYER)
+
+class CruiserModuleSystem(ModuleSystem):
+    """Module system for cruisers"""
+    _SLOT_CONFIG = {
+        "engine": Engine,
+        "armor": Armor,
+        "armament_one": Armament,
+        "armament_two": Armament,
+        "armament_three": Armament
+    }
+
+    def __init__(self):
+        super().__init__(Classification.CRUISER)
+
+class BattleshipModuleSystem(ModuleSystem):
+    """Module system for battleships"""
+    _SLOT_CONFIG = {
+        "engine": Engine,
+        "armor": Armor,
+        "armament_one": Armament,
+        "armament_two": Armament,
+        "armament_three": Armament,
+        "armament_four": Armament
+    }
+
+    def __init__(self):
+        super().__init__(Classification.BATTLESHIP)
+
 class Ship:
     """The core unit of the fleet; stats are determined by the modules"""
     _name: str
     _classification: Classification
-    _modules: dict[Module, int] = {}    # value represents quantity as some modules can have multiple equipped
+    _modules: ModuleSystem
     
     _health: int = 0
     _armor: int = 0
@@ -22,6 +176,28 @@ class Ship:
     def __init__(self, name: str, classification: Classification):
         self._name = name
         self._classification = classification
+        if classification is Classification.DESTROYER:
+            self._modules = DestroyerModuleSystem()
+        elif classification is Classification.CRUISER:
+            self._modules = CruiserModuleSystem()
+        elif classification is Classification.BATTLESHIP:
+            self._modules = BattleshipModuleSystem()
+
+
+    def equip(self, module: Module, target_slot: str):
+        self.modules.equip(module, target_slot)
+
+        for slot in self.modules.equipped_modules.values():
+            if not slot:
+                continue
+            
+            if isinstance(slot, Engine):
+                pass
+            elif isinstance(slot, Armament):
+                self._attack += slot.damage
+            elif isinstance(slot, Armor):
+                self._armor += slot.protection
+        
 
     @property
     def name(self)->str:
@@ -44,7 +220,7 @@ class Ship:
         return self._attack
     
     @property
-    def modules(self)->dict[Module, int]:
+    def modules(self)->ModuleSystem:
         return self._modules
     
     def __str__(self)->str:
@@ -52,28 +228,3 @@ class Ship:
     
     def __repr__(self)->str:
         return f'Ship(name={self.name!r}, classification={self.classification}, health={self.health}, armor={self.armor}, modules={self.modules})'
-
-
-class Module(ABC):
-    """Base class for ship modules which determine ship statistics"""
-    _name: str
-    _type: str
-
-    @abstractmethod
-    def __init__(self, name: str, type: str):
-        self._name = name
-        self._type = type
-
-    @property
-    def name(self)->str:
-        return self._name
-    
-    @property
-    def type(self)->str:
-        return self._type
-    
-    def __str__(self):
-        return self.name
-    
-    def __repr__(self):
-        return f'name={self.name}, type={self.type}'
--- test/test_ship.py
+++ test/test_ship.py
@@ -1,7 +1,8 @@
 import pytest
-from src.ship import Ship, Module, Classification
+from src.ship import Ship, Module, Classification, Engine, Armament, Armor
+from typing import Type
 
-@pytest.fixture
+@pytest.fixture()
 def ship_one()->Ship:
     return Ship("Test", Classification.DESTROYER) 
 
@@ -13,7 +14,6 @@ def ship_two()->Ship:
 def ship_three()->Ship:
     return Ship("New Ship", Classification.BATTLESHIP)
 
-
 @pytest.mark.parametrize("ship_fixture, exp_name, exp_classification, exp_armor, exp_attack, exp_health", [
     ("ship_one", "Test", Classification.DESTROYER, 0, 0, 0),
     ("ship_two", "", Classification.CRUISER, 0, 0, 0),
@@ -22,7 +22,7 @@ def ship_three()->Ship:
 
 def test_ship_instantiation(request, ship_fixture: str, exp_name: str, exp_classification: Classification, exp_armor: int, exp_attack: int, exp_health: int):
     """Test correct instantiation of Ship class and attributes"""
-    ship = request.getfixturevalue(ship_fixture)
+    ship: Ship = request.getfixturevalue(ship_fixture)
 
     assert ship.name == exp_name
     assert ship.classification == exp_classification
@@ -30,14 +30,63 @@ def test_ship_instantiation(request, ship_fixture: str, exp_name: str, exp_class
     assert ship.attack == exp_attack
     assert ship.health == exp_health
 
-@pytest.mark.parametrize("ship_fixture, repr", [
-    ("ship_one", "Ship(name='Test', classification=Classification.DESTROYER, health=0, armor=0, modules={})"),
-    ("ship_two", "Ship(name='', classification=Classification.CRUISER, health=0, armor=0, modules={})"),
-    ("ship_three", "Ship(name='New Ship', classification=Classification.BATTLESHIP, health=0, armor=0, modules={})")
+@pytest.mark.parametrize("ship_fixture, exp_parts", [
+    ("ship_one", ["name='Test'", "classification=Classification.DESTROYER", "health=0", "armor=0", "modules=ModuleSystem"]),
+    ("ship_two", ["name=''", "classification=Classification.CRUISER", "health=0", "armor=0", "modules=ModuleSystem"]),
+    ("ship_three", ["name='New Ship'", "classification=Classification.BATTLESHIP", "health=0", "armor=0", "modules=ModuleSystem"])
 ])
 
-def test_ship_repr(request, ship_fixture: str, repr: str):
-    """Test __repr__ method of the Ship class"""
-    ship = request.getfixturevalue(ship_fixture)
+def test_ship_repr(request, ship_fixture: str, exp_parts: list[str]):
+    """Test key elements of the Ship class __repr__ method"""
+    ship: Ship = request.getfixturevalue(ship_fixture)
 
-    assert ship.__repr__() == repr
+    for part in exp_parts:
+        assert part in ship.__repr__()
+
+@pytest.fixture
+def module_one()->Engine:
+    return Engine("Test Engine", [Classification.DESTROYER], 10)
+
+@pytest.fixture
+def module_two()->Armor:
+    return Armor("Test Armor", [Classification.DESTROYER, Classification.CRUISER], 20)
+
+@pytest.fixture
+def module_three()->Armament:
+    return Armament("Test Weapon", [Classification.DESTROYER, Classification.BATTLESHIP], 15)
+
+@pytest.mark.parametrize("module_fixture, exp_name, exp_ships_list, exp_stats", [
+    ("module_one", "Test Engine", [Classification.DESTROYER], 10),
+    ("module_two", "Test Armor", [Classification.DESTROYER, Classification.CRUISER], 20),
+    ("module_three", "Test Weapon", [Classification.DESTROYER, Classification.BATTLESHIP], 15)
+])
+
+def test_module_instantiation(request, module_fixture: str, exp_name: str, exp_ships_list: list[Classification], exp_stats: int):
+    module: Module = request.getfixturevalue(module_fixture)
+    assert isinstance(module, Module)
+
+    assert module.name == exp_name
+    assert module.allowed_ships == exp_ships_list
+    if isinstance(module, Engine):
+        assert module.propulsion == exp_stats
+    elif isinstance(module, Armament):
+        assert module.damage == exp_stats
+    elif isinstance(module, Armor):
+        assert module.protection == exp_stats
+
+@pytest.mark.parametrize("ship_fixture, module_fixture, target_slot, exp_stats", [
+    ("ship_one", "module_one", "engine", [0, 0, 0]),    # Equip engine
+    ("ship_one", "module_three", "armament_one", [0, 0, 15]),   # Equip armament
+    ("ship_two", "module_two", "armor", [0, 20, 0]),    # Equip armor
+    ("ship_two", "module_two", "armament_one", [0, 0, 0]),    # Equip in wrong slot
+    ("ship_three", "module_two", "armor", [0, 0, 0]),    # Wrong ship type
+    ("ship_one", "module_one", "none", [0,0,0])    # Slot does not exist
+])
+
+def test_equip_method(request, ship_fixture: str, module_fixture: str, target_slot: str, exp_stats: list[int]):
+    ship: Ship = request.getfixturevalue(ship_fixture)
+    module: Module =  request.getfixturevalue(module_fixture)
+    ship.equip(module, target_slot)
+    assert exp_stats[0] == ship.health
+    assert exp_stats[1] == ship.armor
+    assert exp_stats[2] == ship.attack
